0aecf448e10359b95fd3a7164618a496bf358d78
Serge <you@example.com>
2025-05-29 18:44:00 +0200

    Init

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter.tix import INTEGER
+
+message = "Hello"
+print(message)
+
+message = message + "world"
+print(message)
+
+#Integer
+counter = 2
+print(counter)
+
+#check variable type
+variable1 = "text"
+print(type(variable1))
